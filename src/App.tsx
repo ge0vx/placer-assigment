371e3bd98ea4ff88d7e0d6692fed24076baba151
ge0vx <geovapb@gmail.com>
2023-10-07 16:22:19 -0700

    re-order code for codeSandBox

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { TokenContext } from "./context/TokenContext";
 import { useUniversalTutorial } from "./hooks/useUniversalTutorial";
 
 const App: React.FC = () => {
+  //initial configurations
   const { token, tokenLoading, tokenErrorMessage } = useUniversalTutorial();
 
   return (
@@ -16,7 +17,7 @@ const App: React.FC = () => {
       <div className="app-container">
         <div className={`${tokenLoading ? 'loading' : 'not-loading'}`}>loading...</div>
         <div className={`${tokenErrorMessage ? 'error' : 'not-error'}`}>{tokenErrorMessage}</div>
-        { token && <SignUp />}
+        { !tokenLoading && !!token && <SignUp />}
       </div>
     </TokenContext.Provider>
   );
