658263ffe9d162b18cbbed64d1517e4d51abf480
ge0vx <geovapb@gmail.com>
2023-10-07 15:37:43 -0700

    format code for codesanbox

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,29 @@ import { useUniversalTutorial } from "./hooks/useUniversalTutorial";
 const App: React.FC = () => {
   const { token, tokenLoading, tokenErrorMessage } = useUniversalTutorial();
 
+  if (token) {
+    return (
+      <TokenContext.Provider
+        value={{
+          token: token ?? null,
+        }}
+      >
+        <div className="app-container">
+          <SignUp />
+        </div>
+      </TokenContext.Provider>
+    );
+  }
+
   return (
-    <TokenContext.Provider
-      value={{
-        token: token ?? null
-      }}
-    >
-      <div className="app-container">
-        <div className={`${tokenLoading ? 'loading' : 'not-loading'}`}>loading...</div>
-        <div className={`${tokenErrorMessage ? 'error' : 'not-error'}`}>{tokenErrorMessage}</div>
-        {token ? <SignUp /> : <div></div>}
+    <div className="app-container">
+      <div className={`${tokenLoading ? "loading" : "not-loading"}`}>
+        loading...
       </div>
-    </TokenContext.Provider>
+      <div className={`${tokenErrorMessage ? "error" : "not-error"}`}>
+        {tokenErrorMessage}
+      </div>
+    </div>
   );
 };
 
